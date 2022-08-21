275e68dababd549a7aeebba7e174536973a8bdd5
jungsoo <sunpl13@naver.com>
2022-08-21 20:28:54 +0900

    api intercepter 함수

---
+++ src/api/interceptor.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { getAccessToken } from "utils/tokenProvider";
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+/*
+    1. 요청 인터셉터를 작성합니다.
+    2개의 콜백 함수를 받습니다.
+
+    1) 요청 바로 직전 - 인자값: axios config
+    2) 요청 에러 - 인자값: error
+*/
+instance.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+
+    try {
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (err) {
+      throw new Error(err);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+/*
+    2. 응답 인터셉터를 작성합니다.
+    2개의 콜백 함수를 받습니다.
+
+    1) 응답 정성 - 인자값: http response
+    2) 응답 에러 - 인자값: http error
+*/
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    alert(error.response.data.message);
+    return Promise.reject(error);
+  }
+);
+
+// 생성한 인스턴스를 익스포트 합니다.
+export default instance;
